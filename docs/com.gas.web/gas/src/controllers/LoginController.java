e0c636baa372ded8c93d2c8d9be040c06558d4b5
dieci <diegocipa1@gmail.com>
2020-01-19 21:49:57 +0100

    ho messo nuovo dashboard dentro progetto, iniziato primo collegamento con database

--- docs/com.gas.web/gas/src/controllers/LoginController.java
+++ docs/com.gas.web/gas/src/controllers/LoginController.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import business.AuthenticationManager;
 import model.User;
@@ -15,7 +16,7 @@ import model.User;
 /**
  * Servlet implementation class LoginController
  */
-@WebServlet("/admin")
+@WebServlet("/dashboard")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -31,7 +32,6 @@ public class LoginController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		AuthenticationManager am = new AuthenticationManager();
 		User u = am.login(request.getParameter("username"), request.getParameter("password"));
 		if (u == null) {
@@ -39,7 +39,9 @@ public class LoginController extends HttpServlet {
 			out.println("sbagliato");
 			//request.getRequestDispatcher("/").forward(request, response);
 		} else {
-			request.getRequestDispatcher("/login/admin/admin.html").forward(request, response);
+			HttpSession session = request.getSession();
+			session.setAttribute("nome", u.getNome());
+			request.getRequestDispatcher("/login/admin/dashboard.jsp").forward(request, response);
 		}
 	}
 
