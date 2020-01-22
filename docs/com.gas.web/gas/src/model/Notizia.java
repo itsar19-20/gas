914d5759db746aa2ac8cf15ba793c64ccd3ca40d
dieci <diegocipa1@gmail.com>
2020-01-22 17:03:17 +0100

    jpa annotations

--- docs/com.gas.web/gas/src/model/Notizia.java
+++ docs/com.gas.web/gas/src/model/Notizia.java
@@ -16,6 +16,7 @@ public class Notizia implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	@Temporal(TemporalType.DATE)
