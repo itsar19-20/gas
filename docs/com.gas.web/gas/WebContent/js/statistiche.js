625af24f92492164ab165c3791e2a19e311177c1
dieci <diegocipa1@gmail.com>
2020-02-05 16:53:18 +0100

    pulizia

--- docs/com.gas.web/gas/WebContent/js/statistiche.js
+++ docs/com.gas.web/gas/WebContent/js/statistiche.js
@@ -1,52 +1,47 @@
-$(() =>{
-    var dataPoints=[];
+$(() => {
+	var dataPoints = [];
 
-var options =  {
-	animationEnabled: true,
-	theme: "light1",
-	title: {
-		text: "Andamento prezzi",
-		fontFamily: "tahoma"
-	},
-	axisX: {
-		valueFormatString: "YYYY MM DD",
-	},
-	axisY: {
-		title: "Prezzi",
-		titleFontSize: 24,
-		includeZero: false
-	},
-	data: [{
-		type: "spline",
-		yValueFormatString: "$#,###.##",
-		dataPoints: dataPoints
-	}]
-};
+	var options = {
+		animationEnabled: true,
+		theme: "light1",
+		title: {
+			text: "Andamento prezzi",
+			fontFamily: "tahoma"
+		},
+		axisX: {
+			valueFormatString: "YYYY MM DD",
+		},
+		axisY: {
+			title: "Prezzi",
+			titleFontSize: 24,
+			includeZero: false
+		},
+		data: [{
+			type: "spline",
+			yValueFormatString: "$#,###.##",
+			dataPoints: dataPoints
+		}]
+	};
 
 	$.ajax({
 		url: '/gas/OttieniStat',
 		method: 'get'
 	})
-	.done((stat) => {
-		console.log('ciao');
-		if(stat){
+		.done((stat) => {
+			if (stat) {
 
-            for (var i = 0; i < stat.length; i++) {
-            	console.log(stat);
-            	console.log(stat[i].dataComunicazione);
-            	console.log(stat[i].prezzo);
+				for (var i = 0; i < stat.length; i++) {
+					dataPoints.push({
+						x: new Date(stat[i].dataComunicazione),
 
-                dataPoints.push({
-                    x: new Date(stat[i].dataComunicazione),
-                    
-                    y: stat[i].prezzo
-                });
-            }
-            
-           
-        }
-		
-        $("#chartContainer").CanvasJSChart(options);
-        $("#chartContainer").render;
-	})
+						y: stat[i].prezzo
+					});
+				}
+
+
+			}
+
+			$("#chartContainer").CanvasJSChart(options);
+			$("#chartContainer").render;
+		})
 })
