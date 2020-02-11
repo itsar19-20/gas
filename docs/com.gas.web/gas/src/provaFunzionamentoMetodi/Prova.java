bba551dfe36be96832e713a30d996996cbdb918b
dieci <diegocipa1@gmail.com>
2020-02-11 12:04:13 +0100

    pulizia

--- docs/com.gas.web/gas/src/provaFunzionamentoMetodi/Prova.java
+++ docs/com.gas.web/gas/src/provaFunzionamentoMetodi/Prova.java
@@ -2,15 +2,10 @@ package provaFunzionamentoMetodi;
 
 import javax.persistence.EntityManager;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import model.Prezzo;
 import utils.JPAUtil;
 
 public class Prova {
-	// static final Logger LOG = LoggerFactory.getLogger(Prova.class);
-	private static final Logger logger = LoggerFactory.getLogger(Prova.class);
 
 	public static void main(String[] args) {
 
@@ -21,9 +16,7 @@ public class Prova {
 				"Select c FROM Prezzo c WHERE c.descCarburante LIKE :descCarburante AND c.isSelf LIKE : isSelf AND c.dtComu LIKE : dtComu")
 				.setParameter("descCarburante", "Benzina").setParameter("isSelf", 1)
 				.setParameter("dtComu", "01/01/2018 10:20:52").getSingleResult();
-		logger.info("logger logger arrivato");
 		System.out.println(u);
-		logger.info("logger logger prova");
 //		  List<Integer> l=(em.createQuery("Select c.idImpianto FROM Distributore c", Integer.class).getResultList());
 //		  int i=0;
 //		 for (i=0;i<l.size();i++) {
