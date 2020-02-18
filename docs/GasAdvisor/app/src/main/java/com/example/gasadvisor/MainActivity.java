f3febdc63d9e578e8ec7d9b155d2f4e80a4dd2aa
dieci <diegocipa1@gmail.com>
2020-02-18 07:09:28 +0100

    parte 2 nav drawer

--- docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/MainActivity.java
+++ docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.Menu;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -14,11 +18,20 @@ import com.mapbox.mapboxsdk.maps.Style;
 
 public class MainActivity extends AppCompatActivity {
     private MapView mapView;
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, "pk.eyJ1IjoiZGllZ29jaXBhIiwiYSI6ImNrNXozOGRrOTA1eGczZ3FqazR2amthb2UifQ.Kyzu-TDi7MYZPhBe4s_WqA");
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
@@ -27,51 +40,67 @@ public class MainActivity extends AppCompatActivity {
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
-
 // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
-
-
-
                     }
                 });
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        //serve quando clichiamo indietro dal telefono, non
+        //lasciare l`activity ma solo chiudere drawer
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+            //se drawer fosse a destra scrivi GravityCompat.END
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     @Override
     public void onStart() {
         super.onStart();
         mapView.onStart();
     }
+
     @Override
     public void onResume() {
         super.onResume();
         mapView.onResume();
     }
+
     @Override
     public void onPause() {
         super.onPause();
         mapView.onPause();
     }
+
     @Override
     public void onStop() {
         super.onStop();
         mapView.onStop();
     }
+
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         return true;
