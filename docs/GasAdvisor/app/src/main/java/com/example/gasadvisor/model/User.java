e4c9e9d9aee5c3cec4abf45778e3749220ff365f
dieci <diegocipa1@gmail.com>
2020-02-23 13:16:38 +0100

    inizio sqlite helper e adapter

---
+++ docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/model/User.java
@@ -0,0 +1,41 @@
+package com.example.gasadvisor.model;
+
+public class User {
+    Integer _id;
+    String username;
+    String password;
+    String email;
+    String name;
+    String lastName;
+
+    public User(String username, String email, String name, String lastName) {
+        this.username = username;
+        this.email = email;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Integer get_id() {
+        return _id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
