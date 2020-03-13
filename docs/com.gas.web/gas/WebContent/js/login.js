46140bfc542c01811b4dae1719b9d622a49d0cf0
dieci <diegocipa1@gmail.com>
2020-03-13 12:01:04 +0100

    modifiche path

--- docs/com.gas.web/gas/WebContent/js/login.js
+++ docs/com.gas.web/gas/WebContent/js/login.js
@@ -9,25 +9,27 @@ $(() => {
             }
         })
             .done((utente) => {
-            	 
+
                 if (utente) {
-                	if ($("input[type=checkbox]").is( 
-                    ":checked")) {
-                    localStorage.setItem('user', JSON.stringify(utente));
-                    localStorage.setItem('remember',1);
-                    location.href = './utenti.html';
-                    
+                    if ($("input[type=checkbox]").is(
+                        ":checked")) {
+                        localStorage.setItem('user', JSON.stringify(utente));
+                        localStorage.setItem('remember', 1);
+                        location.href = './utenti.html';
+
+                    } else {
+                        localStorage.setItem('user', JSON.stringify(utente));
+                        localStorage.setItem('remember', 0);
+                        location.href = './utenti.html';
+
+                    }
                 } else {
-                	localStorage.setItem('user', JSON.stringify(utente));
-                	localStorage.setItem('remember',0);
-                	location.href = './utenti.html';
-                    
-            	  } } else {
-            		  localStorage.removeItem('user');
-                      alert("Questo Utente non puo' procedere!")
-            	  }
-            	}).fail(() => {
+                    localStorage.removeItem('user');
+                    alert("Questo Utente non puo' procedere!")
+                }
+            }).fail(() => {
                 alert("OOPS...Qualcosa e' andato storto!")
-         
-    }); })           
+
+            });
+    })
 });
