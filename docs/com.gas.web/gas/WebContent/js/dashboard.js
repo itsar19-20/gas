779b0dc4dd64a3c786e0d3c5b88eba8a147ecd21
sabbailrosso <sabbathered@gmail.com>
2020-03-13 10:29:12 +0100

    Implementata funzione "Remember me" inn login webapp

--- docs/com.gas.web/gas/WebContent/js/dashboard.js
+++ docs/com.gas.web/gas/WebContent/js/dashboard.js
@@ -1,14 +1,22 @@
 //Verifica che Admin sia loggato altrimenti manda in Login
 $(() => {
-    if (localStorage.getItem('user')) {
-        var utente = JSON.parse(localStorage.getItem('user'));
-        $('#nomeAdmin').text(`Benvenuto ${utente.nome}`);
-    } else {
-        location.href = './login.html';
-    }
+	var remember = localStorage.getItem('remember');
+	var utente = JSON.parse(localStorage.getItem('user'));
+	alert(remember);
+	if (localStorage.getItem('user')) {
+		$('#nomeAdmin').text(`Benvenuto ${ utente.nome }`);
+		if (remember == 0) {
+			window.onbeforeunload = function() {
+				localStorage.removeItem('user');
+				  return '';
+				};
+		}
+	} else {
+		location.href = './login.html';
+	}
 });
-//bottone logout per tutti i html che usano il dashboard
+// bottone logout per tutti i html che usano il dashboard
 $('#logout').click(() => {
-    localStorage.removeItem('user');
-    location.href = './index.html';
+	localStorage.removeItem('user');
+	location.href = './index.html';
 });
--- docs/com.gas.web/gas/src/provaFunzionamentoMetodi/Prova.java
+++ docs/com.gas.web/gas/src/provaFunzionamentoMetodi/Prova.java
@@ -1,16 +1,16 @@
-package provaFunzionamentoMetodi;
-
-import java.util.List;
-
-import business.DistributoreManager;
-import model.Prezzo;
-
-public class Prova {
-	public static void main(String[] args) {
-		
-		DistributoreManager dm = new DistributoreManager();
-		List<Prezzo> lista = dm.cercaPiuEconomici("Benzina");
-		System.out.println(lista.size());
-	}
-
-}
+//package provaFunzionamentoMetodi;
+//
+//import java.util.List;
+//
+//import business.DistributoreManager;
+//import model.Prezzo;
+//
+//public class Prova {
+//	public static void main(String[] args) {
+//		
+//		DistributoreManager dm = new DistributoreManager();
+//		List<Prezzo> lista = dm.cercaPiuEconomici("Benzina");
+//		System.out.println(lista.size());
+//	}
+//
+//}
