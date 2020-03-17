50c7232c2c844c5d9c679f746a12df6ed5ffae4b
dieci <diegocipa1@gmail.com>
2020-03-17 09:29:01 +0100

    SES

--- docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/view/SettingsActivity.java
+++ docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/view/SettingsActivity.java
@@ -13,10 +13,10 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+       // setContentView(R.layout.settings_activity);
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.settings, new SettingsFragment())
+      //          .replace(R.id.settings, new SettingsFragment())
                 .commit();
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -27,7 +27,7 @@ public class SettingsActivity extends AppCompatActivity {
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        //    setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
 }
--- docs/com.gas.web/gas/src/utils/MailUtils.java
+++ docs/com.gas.web/gas/src/utils/MailUtils.java
@@ -1,16 +1,11 @@
 package utils;
 
 import java.util.Properties;
-import java.util.Random;
 import java.util.UUID;
 
-import javax.mail.Authenticator;
 import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -18,44 +13,46 @@ public class MailUtils {
 	public String sendNewPass(String email) {
 		String newPass = UUID.randomUUID().toString().replace("-", "");
 		newPass = newPass.substring(0, Math.min(newPass.length(), 10));
-		Properties properties = new Properties();
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.host", "smtp.gmail.com");
-		properties.put("mail.smtp.port", "587");
 
-		String myAccount = "loscoiattolodellabenzina@gmail.com";
+		String FROM = "loscoiattolodellabenzina@gmail.com";
+		String FROMNAME = "ses-smtp-user.20200316-222900";
 		String myPass = "G454DV1S0R";
-
-		Session session = Session.getInstance(properties, new Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(myAccount, myPass);
-			}
-		});
-		Message message = prepareMessage(session, myAccount, email, newPass);
+		String SMTP_USERNAME = "AKIAUMAHUHQRIYF2B56A";
+		String SMTP_PASSWORD = "BOSq6ld9CqwST/hc2W5MmTnzRvcOxSmmCPbe0dCarxTi";
+		String HOST = "email.eu-central-1.amazonaws.com";
+		String SUBJECT = "GasAdvisor Password Recovery";
+		String BODY = String.join(System.getProperty("line.separator"), "<h1>GasAdvisor Password Recovery:</h1>",
+				"<p>Il suo nuovo password e' " + newPass + " </p>");
+		String TO = email;
+		int PORT = 587;
+		Properties props = System.getProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.port", PORT);
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.auth", "true");
+		Session session = Session.getDefaultInstance(props);
+		Message msg = new MimeMessage(session);
 		try {
-			Transport.send(message);
-			return newPass;
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	private static Message prepareMessage(Session session, String myAccount, String email, String newPass) {
-		try {
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(myAccount));
-			message.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
-			message.setSubject("Email Recovery");
-			message.setText("Il suo nuovo passord e': " + newPass);
-			return message;
+			msg.setFrom(new InternetAddress(FROM, FROMNAME));
+			System.out.println("1");
+			msg.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
+			System.out.println("2");
+			msg.setSubject(SUBJECT);
+			System.out.println("3");
+			msg.setContent(BODY, "text/html");
+			Transport transport = session.getTransport();
+			System.out.println("4");
+			transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);
+			System.out.println("5");
+			transport.sendMessage(msg, msg.getAllRecipients());
+			System.out.println("6");
+			transport.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println(e.getLocalizedMessage());
+			return null;
 		}
-		return null;
+		return newPass;
+
 	}
-	
 
 }
