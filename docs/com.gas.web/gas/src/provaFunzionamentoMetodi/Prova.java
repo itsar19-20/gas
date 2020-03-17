84c2a18e2787048f862d2aae95089f9f53d98bcf
dieci <diegocipa1@gmail.com>
2020-03-17 19:48:17 +0100

    finito lavoro di simone, 1 valutazione per utente

--- docs/com.gas.web/gas/src/provaFunzionamentoMetodi/Prova.java
+++ docs/com.gas.web/gas/src/provaFunzionamentoMetodi/Prova.java
@@ -1,16 +1,24 @@
-//package provaFunzionamentoMetodi;
-//
-//import java.util.List;
-//
-//import business.DistributoreManager;
-//import model.Prezzo;
-//
-//public class Prova {
-//	public static void main(String[] args) {
-//		
-//		DistributoreManager dm = new DistributoreManager();
-//		List<Prezzo> lista = dm.cercaPiuEconomici("Benzina");
-//		System.out.println(lista.size());
-//	}
-//
-//}
+package provaFunzionamentoMetodi;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import business.DistributoreManager;
+import model.Prezzo;
+import model.Valutazione;
+import utils.JPAUtil;
+
+public class Prova {
+	public static void main(String[] args) {
+		EntityManager em = JPAUtil.getInstance().getEmf().createEntityManager();
+		List<Valutazione> list = em.createQuery("select d from Valutazione d where d.user.nome like:name and d.distributore.idImpianto like:idImpianto", Valutazione.class)
+				.setParameter("name", "simone").setParameter("idImpianto", 12629).getResultList();
+		
+//		System.out.println(list);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i).getDescrizione());
+		}
+	}
+
+}
