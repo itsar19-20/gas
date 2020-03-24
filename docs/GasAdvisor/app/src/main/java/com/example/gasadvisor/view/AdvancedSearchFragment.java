8913db4c0508d780c38fda2a3ebae8a9500cfd82
sabbailrosso <sabbathered@gmail.com>
2020-03-24 17:34:35 +0100

    Cambio tema funzionante

--- docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/view/AdvancedSearchFragment.java
+++ docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/view/AdvancedSearchFragment.java
@@ -1,5 +1,7 @@
 package com.example.gasadvisor.view;
 
+import android.content.SharedPreferences;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,23 +14,29 @@ import androidx.fragment.app.Fragment;
 
 import com.example.gasadvisor.R;
 
+import static com.mapbox.mapboxsdk.Mapbox.getApplicationContext;
+
 public class AdvancedSearchFragment extends Fragment {
     private Button btnPiuVicino;
+    SharedPreferences ColorPreference = getApplicationContext().getSharedPreferences("color", 0);
+    Boolean isDark = ColorPreference.getBoolean("isDark", true);
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v =  inflater.inflate(R.layout.fragment_advanced, container, false);
-    btnPiuVicino = v.findViewById(R.id.btn_piuVicino_avanzFrag);
-    btnPiuVicino.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            getActivity().setResult(0);
-            getActivity().finish();
-        }
-    });
+        View v = inflater.inflate(R.layout.fragment_advanced, container, false);
+
+        btnPiuVicino = v.findViewById(R.id.btn_piuVicino_avanzFrag);
+        btnPiuVicino.setBackgroundTintMode(PorterDuff.Mode.LIGHTEN);
+        btnPiuVicino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().setResult(0);
+                getActivity().finish();
+            }
+        });
 
 
-    return v;
+        return v;
     }
 }
--- docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/view/SettingsActivity.java
+++ docs/GasAdvisor/app/src/main/java/com/example/gasadvisor/view/SettingsActivity.java
@@ -1,11 +1,14 @@
 package com.example.gasadvisor.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gasadvisor.R;
 
@@ -13,23 +16,52 @@ public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences ColorPreference = getApplicationContext().getSharedPreferences("color", 0);
+        Boolean isDark = ColorPreference.getBoolean("isDark", true);
+        setTheme(isDark ? R.style.temaScuro : R.style.temaChiaro);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Switch aSwitch = findViewById(R.id.temaSwitch);
+        aSwitch.setTextColor(isDark ? getResources().getColor(R.color.whiteTextColor) : getResources().getColor(R.color.black));
+        try {
+            aSwitch.setChecked(isDark ? true : false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Button button = findViewById(R.id.settinsActivity_backButton);
+        button.setBackgroundTintList(isDark ? ColorStateList.valueOf(getResources().getColor(R.color.whiteCardColor)) : ColorStateList.valueOf(getResources().getColor(R.color.back)));
+        button.setTextColor(!isDark ? ColorStateList.valueOf(getResources().getColor(R.color.whiteCardColor)) : ColorStateList.valueOf(getResources().getColor(R.color.back)));
+        ;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsActivity.this, HomeActivity.class));
+            }
+        });
         aSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                SharedPreferences ColorPreference = getApplicationContext().getSharedPreferences("color", 0);
                 SharedPreferences.Editor editorPref = ColorPreference.edit();
+
                 Boolean switchState = aSwitch.isChecked();
                 if (switchState) {
-                    editorPref.putInt("tema", R.style.AppTheme);
+                    editorPref.putBoolean("isDark", true);
+                    setTheme(R.style.temaScuro);
                 } else {
-                    editorPref.putInt("tema", R.style.AppThemeLight);
+                    editorPref.putBoolean("isDark", false);
+                    setTheme(R.style.temaChiaro);
                 }
-                editorPref.commit();
+                editorPref.apply();
+                Intent intent = new Intent(SettingsActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(SettingsActivity.this, HomeActivity.class));
     }
 }
